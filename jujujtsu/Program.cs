2cb8603e2d392b9effc97162910c9707ddc6714d
miqueasjunior <miqueas.junior@escolar.ifrn.edu.br>
2024-08-12 16:16:18 -0300

    tarefa

---
+++ jujujtsu/Program.cs
@@ -0,0 +1,47 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("digite a nota");
+        int nota1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("digite a nota");
+        int nota2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("digite a nota");
+        int nota3  = int.Parse(Console.Readline());
+
+        Console.WriteLine("digite a nota");
+        int nota4 = int.Parse(Console.ReadLine());
+
+        float media = (2 * nota2 + *nota2 + 3 * nota3 + 3 * nota 4)/  10f;
+        Console.WriteLine("media da disciplina:" + media);
+if (media>= 60){
+    Console.WriteLine("aprovado");
+    return ;
+}
+    else if (media<20){
+        Console.WriteLine("reprovado");
+    
+        return;
+   else {
+    Console.WriteLine("recuperação');
+   }
+   Console.WriteLine("digite o valor da prova final:");
+   int provaFinal = int. Parse (Console.ReadLine());
+
+   float mediaFinal= (média + provaFinal)/2f;
+   Console.WriteLine("mediafinal:"+ mediaFinal);
+            if ("médiafinal": < 60){
+            {
+                Console.WriteLine("reprovado");
+            }
+            else
+            
+                Console.WriteLine("aprovado");
+            
+
+
+
+
+
